53b29d039cb3b473cca7d74c1de3672290dc4b4a
Juwon <hardejuwon@gmail.com>
2023-06-14 13:56:28 +0100

    all views display properly

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,18 +2,28 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
   after_save :update_posts_counter
+
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def five_latest_comments
+    comments.order('created_at DESC').limit(5)
+  end
+
+  def comments_counter
+    comments.count
+  end
+
+  def likes_counter
+    likes.count
+  end
+
   private
 
   def update_posts_counter
-    author.increment!(:posts_counter)
-  end
-
-  def five_latest_comments
-    comments.order('created_at DESC').limit(5)
+    author.update(posts_counter: author.posts.count)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
+
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def posts_counter
+    posts.count
+  end
+
   def three_latest_posts
-    posts.order('created_at DESC').limit(3)
+    posts.where(author: self).order(created_at: :desc).limit(3)
   end
 end
+
