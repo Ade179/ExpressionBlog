d8f0a40d59aaffd6285e8b9b87335eb232fe7f3e
Juwon <hardejuwon@gmail.com>
2023-06-19 11:03:21 +0100

    fix error

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(comments: [:author])
+  end
+
+  def show
+    @post = Post.includes(comments: [:author]).find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(post_data)
+    post.likes_counter = 0
+    post.comments_counter = 0
+
+    respond_to do |format|
+      format.html do
+        if post.save
+          redirect_to "/users/#{post.author_id}/posts/", notice: 'Success Post Saved!'
+        else
+          render :new, status: 'Error occured with Post!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_data
+    params.require(:post).permit(:title, :text)
+  end
+end
